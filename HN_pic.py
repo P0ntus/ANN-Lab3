214c418062428995588ac69f5166f16939b7b0d4
Ayoub Bargach <bargach@kth.se>
2018-03-19 14:06:25 +0100

    Part 2

--- HN_pic.py
+++ HN_pic.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+from __future__ import division
 import os
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Some usefull functions
+# Some useful functions
 
 # Set any value in x >= 0 to 1 and any value < 0 to -1
 def sgn(x):
@@ -16,9 +17,9 @@ def sgn(x):
 
 # To update the weights
 # As the matrix is symetric with 0 in the diagonal, we will only fill one triangle
-def weights_update(weights, patterns) :
+def weights_update_origin(weights, patterns) :
 	for i in range( 0, len( weights )) :
-		for j in range( i+1 , len( weights )) :
+		for j in range( i , len( weights )) :
 			result = 0
 			# We take the first pattern dimension, in the begun case, it was 32 lines (in 2D pict).
 			dim = len( patterns[0] )
@@ -27,6 +28,11 @@ def weights_update(weights, patterns) :
 			weights[i][j] = ( 1 / len( weights ) ) * result
 			# The matrix is symetric, we update also the other triangle
 			weights[j][i] = ( 1 / len( weights ) ) * result
+	# print( weights )
+	return weights
+
+def weights_update(patterns, number) :
+	return np.matmul(patterns[:number].T, patterns[:number])
 
 # To generate the next pattern from the weights
 def patterns_update( weights, patterns ):
@@ -38,6 +44,16 @@ def patterns_update( weights, patterns ):
 			for m in range( 0, len( weights )) :
 				result += weights[l][m] * save_pattern[ m // dim][ m % dim ]
 			patterns[i][ l // dim][ l % dim ] = sgn( result )
+	return patterns
+
+# To generate the next pattern from the weights
+def get_pattern( weights, pattern ):
+	for l in range( 0, len( pattern )) :
+		result = 0
+		for m in range( 0, len( weights )) :
+			result += weights[l][m] * pattern[ m ]
+		pattern[l] = sgn( result )
+	return pattern
 	
 
 # We format the given data from animals.dat
@@ -50,13 +66,21 @@ pict_f.close()
 
 raw_pict = pict_a.split(",")
 
+#print("RAW DATA LENGTH")
+#print( len(raw_pict) )
+
 # Some Constants
 epochs = 10
 pict_def = 32
-number_patterns = 9
+number_patterns = 11
 number_nodes = 1024
 
-pict = [ [ [ int( raw_pict[j + i * pict_def + k * number_patterns] ) for j in range(0, pict_def) ] for i in range(0, pict_def) ] for k in range(0, number_patterns) ]
+# Figure constants
+rows = 3
+columns = 2
+
+# pict = [ [ [ int( raw_pict[j + i * pict_def + k * number_patterns] ) for j in range(0, pict_def) ] for i in range(0, pict_def) ] for k in range(0, number_patterns) ]
+pict = [ [ int( raw_pict[j  + i * number_nodes] ) for j in range(0, number_nodes) ] for i in range(0, number_patterns) ]
 
 # print( pict )
 # plt.imshow(pict[3])
@@ -65,38 +89,63 @@ pict = [ [ [ int( raw_pict[j + i * pict_def + k * number_patterns] ) for j in ra
 
 # HERE the data is ready --- 9 patterns of 32x32 pict matrix
 # INIT the weights matrix for N nodes, so we have N^2 weights
-weights = np.zeros( (number_nodes, number_nodes) , dtype = int)
+weights = np.zeros( (number_nodes, number_nodes) , dtype = float)
 
 save_pict = pict
 
 
-# We udpate the weights with the inital patterns
-weights_update(weights, pict)
+# convert
+pict = np.array( pict )
 
+
+# We udpate the weights with the inital patterns
+# weights = weights_update(weights, pict)
 
 # LEARNING
-for e in range( 0, epochs) :
-	patterns_update(weights, pict)
-	weights_update(weights, pict)
-	
+weights = weights_update( pict, 3 )
+
+
+# First CALL
+print( pict.shape )
+for i in range( 0, len(pict) ) :
+	pict[i] = get_pattern(weights, pict[i])
+
 
 # TESTING
 # Here we verify if the patterns are stable :
-if np.array_equal(save_pict[:3], patterns_update(weights, save_pict[:3])) == True :  # test if same shape, same elements values, only for the 3 first
+if np.array_equal(save_pict[:3], pict[:3]) == True :  # test if same shape, same elements values, only for the 3 first
 	print( "The patterns are stable !" )
 else :
 	print( "UNSTABLE" )
 
-plt.figure()
 
-plt.imshow(save_pict[0])
-plt.imshow(pict[0])
+plot_save_pict = []
+plot_pict = []
 
-plt.imshow(save_pict[1])
-plt.imshow(pict[1])
+# Plot format
+for i in range( 0, len(pict) ) :
+	plot_save_pict.append( [ [ int( save_pict[i][j  + k * pict_def] ) for j in range(0, pict_def) ] for k in range(0, pict_def) ] )
 
-print( pict[0] )
-print(save_pict[0])
+for i in range( 0, len(pict) ) :
+	plot_pict.append( [ [ int( pict[i][j  + k * pict_def] ) for j in range(0, pict_def) ] for k in range(0, pict_def) ] )
+
+
+
+# Plot print : 
+fig = plt.figure()
+
+for i in range( 1 , 4 ) : # (rows*columns//2) + 1
+	fig.add_subplot(rows, columns, i*2 - 1)
+	plt.imshow(plot_save_pict[i - 1])
+
+for i in range( 1 , 4 ) :
+	fig.add_subplot(rows, columns, i*2)
+	# print( save_pict[i] )
+	plt.imshow( plot_pict[i - 1] )
+
+# print( pict[0] )
+# print( weights )
+# print(save_pict[0])
 
 plt.show()
 
